b4afafad2d3b5cdf892aff60f278465bd64de83c
Alex Pooley <zuedev@gmail.com>
2025-02-10 22:47:50 +0000

    setup simple teleporter

---
+++ Assets/ThatEmptySpace/TeleportPlayerToGameObject.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportPlayerToGameObject : UdonSharpBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        Debug.Log("Cube Start");
+    }
+
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(target.transform.position, target.transform.rotation);
+    }
+}
