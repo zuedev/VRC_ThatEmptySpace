fc69d68e729093153336c9db50312362b63bb7d5
Alex Pooley <zuedev@gmail.com>
2025-02-14 04:58:06 +0000

    position sync

---
+++ Assets/Examples/PositionSync/PlayerPositionSyncer.cs
@@ -0,0 +1,45 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class PlayerPositionSyncer : UdonSharpBehaviour
+{
+    // The place to store the player's position and rotation
+    [UdonSynced]
+    private Vector3 _Position;
+    [UdonSynced]
+    private Quaternion _Rotation;
+    
+    // The player that this script is attached to
+    private VRCPlayerApi _owner;
+    
+    // if the data is restored, teleport the player to the restored position
+    public override void OnPlayerRestored(VRCPlayerApi player)
+    {
+        if(!player.isLocal) return;
+        
+        player.TeleportTo(_Position, _Rotation);
+    }
+
+    private void Start()
+    {
+        if (Networking.LocalPlayer.IsOwner(gameObject))
+        {
+            _owner = Networking.LocalPlayer;
+            SendCustomEventDelayedSeconds(nameof(UpdatePosition), 0.5f);
+        }
+    }
+    
+    // get the player's position and rotation every 0.5 seconds and store it in the player data
+    public void UpdatePosition()
+    {
+        if (_owner.IsPlayerGrounded()) // only update the position if the player is grounded
+        {
+            _Position = _owner.GetPosition();
+            _Rotation = _owner.GetRotation();
+            RequestSerialization();
+        }
+        SendCustomEventDelayedSeconds(nameof(UpdatePosition), 0.5f);
+    }
+}
---
+++ Assets/Examples/PositionSync/README.md
@@ -0,0 +1,14 @@
+﻿# Position Sync example
+
+## Description
+A simple PlayerObject prefab which saves each player's position and rotation, then restores them when they rejoin the world.
+
+## Inspector Parameters
+* `string` **Synced Position key** - The key name for the position you want to use from PlayerData.
+* `string` **Synced Rotation key** - The key name for the rotation you want to use from PlayerData.
+
+---
+## How to Use This Example
+1. Add the `PositionSync` prefab to your scene.
+2. Change the `Synced Position key` and `Synced Rotation key` to the PlayerData variable key you want to use for position syncing.
+3. Run Build & Test!
